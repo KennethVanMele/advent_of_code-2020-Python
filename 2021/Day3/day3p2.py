2e6d383d2fea02cb097815011bfef3a4152c9753
Myricht <kennethvanmele@hotmail.com>
2021-12-04 00:26:28 +0100

    day3

--- 2021/Day3/day3p2.py
+++ 2021/Day3/day3p2.py
@@ -1,54 +1,40 @@
-with open('input.txt') as inp:
-    puzzle_input = [line.strip() for line in inp.readlines()]
+def filter_input(mode):
+    with open('input.txt') as inp:
+        puzzle_input = [line.strip() for line in inp.readlines()]
 
-length = len(puzzle_input[0])
-po = puzzle_input
+    length = len(puzzle_input[0])
+    pi = puzzle_input
+    if mode == "oxygen":
+        p1 = '1'
+        p2 = '0'
+    elif mode == "co2":
+        p1 = '0'
+        p2 = '1'
 
-for i in range(length):
-    c0 = 0
-    c1 = 0
-    for j in po:
-        if j[i] == '0':
-            c0 += 1
+    for i in range(length):
+        c0 = 0
+        c1 = 0
+        for j in pi:
+            if j[i] == '0':
+                c0 += 1
+            else:
+                c1 += 1
+
+        if c0 > c1:
+            rem = [p for p in pi if p[i] == p1]
+            for r in rem:
+                pi.remove(r)
         else:
-            c1 += 1
+            rem = [p for p in pi if p[i] == p2]
+            for r in rem:
+                pi.remove(r)
 
-    if c0 > c1:
-        rem = [p for p in po if p[i] == '1']
-        for r in rem:
-            po.remove(r)
-    else:
-        rem = [p for p in po if p[i] == '0']
-        for r in rem:
-            po.remove(r)
+        if len(pi) == 1:
+            break
 
-    if len(po) == 1:
-        break
+    return pi
 
-with open('input.txt') as inp:
-    puzzle_input = [line.strip() for line in inp.readlines()]
 
-pc = puzzle_input
-
-for i in range(length):
-    c0 = 0
-    c1 = 0
-    for j in pc:
-        if j[i] == '0':
-            c0 += 1
-        else:
-            c1 += 1
-
-    if c0 > c1:
-        rem = [p for p in pc if p[i] == '0']
-        for r in rem:
-            pc.remove(r)
-    else:
-        rem = [p for p in pc if p[i] == '1']
-        for r in rem:
-            pc.remove(r)
-
-    if len(pc) == 1:
-        break
-
+po = filter_input("oxygen")
+pc = filter_input("co2")
 print(int(''.join(map(str, po)), 2) * int(''.join(map(str, pc)), 2))
