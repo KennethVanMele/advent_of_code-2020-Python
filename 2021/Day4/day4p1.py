55352b902dc1f05c5fc5ff7e70dc9bf97bb61109
Myricht <kennethvanmele@hotmail.com>
2021-12-08 19:34:50 +0100

    Day4P1

--- 2021/Day4/day4p1.py
+++ 2021/Day4/day4p1.py
@@ -5,7 +5,6 @@ draw = puzzle_input[0]
 draw = draw.split(",")
 board = []
 boards = []
-found = False
 score = 0
 
 for i in range(len(puzzle_input)):
@@ -20,18 +19,42 @@ for i in range(len(puzzle_input)):
             boards.append(board)
             board = []
 
-for i in draw:
-    for b in boards:
-        for r in b:
-            if r.__contains__(i):
-                r.remove(i)
-            if len(r) == 0:
-                for fr in b:
-                    fr = [int(n) for n in fr]
-                    score += sum(fr)
-                found = True
+memo = {}
+for num in draw:
+    memo[num] = []
+for bi, board in enumerate(boards):
+    for ri, row in enumerate(board):
+        for ci, num in enumerate(row):
+            if num in memo:
+                memo[num].append((bi, ri, ci))
 
-    if found:
-        score = score * int(i)
-        print(score)
+
+def is_win(grid, row, col):
+    grid[row][col] = -1
+    rs = [int(n) for n in grid[row]]
+    if sum(rs) == -5:
+        return True
+    rc = [row[col] for row in grid]
+    rc = [int(n) for n in rc]
+    if sum(rc) == -5:
+        return True
+    return False
+
+
+def process_winner(grids, grid_indexes):
+    for gi, ri, ci in grid_indexes:
+        grid = grids[gi]
+
+        if is_win(grid, ri, ci):
+            w = [(0 if grid[i][j] == -1 else grid[i][j]) for i in range(5) for j in range(5)]
+            w = [int(n) for n in w]
+            return sum(w) * int(num)
+    return None
+
+
+for num in draw:
+    score = process_winner(boards, memo[num])
+
+    if score is not None:
         break
+print(f'Answer: {score}')
