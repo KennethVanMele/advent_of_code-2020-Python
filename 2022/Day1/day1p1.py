5d351c95de60b4e52d6d309ccbdaa65f4a3a18bb
Myricht <kennethvanmele@hotmail.com>
2022-12-11 22:45:01 +0100

    Day1

---
+++ 2022/Day1/day1p1.py
@@ -0,0 +1,19 @@
+with open('input.txt') as inp:
+    puzzle_input = [line.strip() for line in inp.readlines()]
+
+elfs = []
+curr_call = 0
+
+try:
+    for i in puzzle_input:
+        if i != '':
+            curr_call += int(i)
+        else:
+            elfs.append(curr_call)
+            curr_call = 0
+# Fix read last line
+finally:
+    elfs.append(curr_call)
+
+print(elfs)
+print(max(elfs))
